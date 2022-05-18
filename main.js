75a02044a36799c689d25ea33b742e0b907daf68
Eduardo <barrosoeduardo64@gmail.com>
2022-05-18 14:23:59 -0300

    finalização do projeto(poderá ser implementado outras funcionalidaes futuramente), com a remoção de bugs

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
-const textsToEnter = [
+const text = [
 
-    'O serviço gratuito do Google traduz instantaneamente palavras', 
-    'frases e páginas da Web entre o inglês e mais de 100 outros idiomas'
-
+    '1. The Power of Habit - Charles Duhigg', 
+    '2. Desbloqueie o poder da sua mente - Michael Arruda',
+    '3. Proteja sua emoção: Aprenda a ter a mente livre e saudável - Augusto Cury',
+    '4. Inteligência Multifocal (Multifocal Intelligence) - Augusto Cury',
+    '5. A teoria do Cisne Negro – Nassim Taleb',
+    '6. A bola de neve – Alice Schroeder',
+    '7. DECONDING SILICON VALLEY – MICHELLE MESSINA',
+    '8. SEM DINHEIRO – BRUNO PERIN',
+    '9. STARTUP WEEKEND – MARC NAGER',
+    '10. Wuthering Heights - Emily Brontë',
+    
 ]
 
-const div = document.querySelector('.digitation')
+const div = document.querySelector('[data-js="type"]')
+
 
 let messageIndex = 0
 let characterIndex = 0
 let currentMessage = ''
 let currentCharacter = ''
 
+
 const type = () => {
-    if(messageIndex === div.length){
+   
+
+   if(messageIndex === text.length){
         messageIndex = 0
+        characterIndex = 0
     }
-    
-    currentMessage = textsToEnter[messageIndex]
+
+        console.log(messageIndex === text.length)
+    currentMessage = text[messageIndex]
     currentCharacter = currentMessage.slice(0, characterIndex++)
     div.textContent = currentCharacter
-    console.log(currentCharacter.length)
+    console.log(currentCharacter)
+
+     
+    
 
     if(currentCharacter.length === currentMessage.length){
         messageIndex++
-        currentCharacter = 0
+        characterIndex = 0
     }
 }
 
-setInterval(type, 200)
+setInterval(type, 150)
